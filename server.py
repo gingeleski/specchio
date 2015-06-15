a491ebd97d7b68c4536a02b2f4280ac3cb9bed97
Randy Gingeleski <rgingeleski@aol.com>
2015-06-15 17:12:32 -0400

    added instructions for dummies

--- server.py
+++ server.py
@@ -5,7 +5,8 @@ PORT = 5000
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+httpd = SocketServer.TCPServer(('', PORT), Handler)
 
-print "serving at port", PORT
+print 'Serving at port', PORT
+print 'Navigate to localhost:' + str(PORT) + ' in the browser'
 httpd.serve_forever()
